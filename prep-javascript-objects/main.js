8cebb6256c71d945738b40b3ed31826081cb5359
codewithbingle <codewithbingle@gmail.com>
2022-01-15 21:45:48 -0800

    Completed prep-javascript-objects Exercise

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,21 @@
+var person = {
+  firstName: 'Bingle',
+  lastName: 'Padilla',
+  hobby: 'Cooking'
+};
+
+console.log(person);
+
+var firstName = 'Bingle';
+var lastName = ' Padilla';
+var fullName = firstName + lastName;
+console.log("The person's name is:", fullName);
+
+person.job = 'Software Engineer';
+console.log("The person's current job is:", person.job);
+
+person.previousJob = 'Licensed Vocational Nurse';
+console.log("The person's previous job is:", person.previousJob);
+
+console.log(person);
+console.log();
