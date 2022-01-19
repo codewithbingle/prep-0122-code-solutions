d4bc1f2a664bc7414e8a76365fbe4b69effaa317
codewithbingle <codewithbingle@gmail.com>
2022-01-18 23:09:52 -0800

    Completed prep-javascript-functions Exercises

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,64 @@
+// addTwoNumbers
+function addTwoNumbers(x, y) {
+  return x + y;
+}
+
+var sum = addTwoNumbers(4, 4);
+console.log('sum', sum);
+
+// convertHoursToMinutes
+function convertHoursToMinutes(hours) {
+  return hours * 60;
+}
+
+var minutes = convertHoursToMinutes(3);
+console.log(minutes);
+
+// getGreeting
+function getGreeting(name) {
+  return 'You can do this,' + name + '!';
+}
+
+getGreeting('Bingle');
+
+// addAndMultiplyBy5
+function addAndMultiplyBy5(x, y) {
+  return (5) * (x + y);
+}
+
+addAndMultiplyBy5(2, 4);
+
+// multiplyAndDivideBy5
+function multiplyAndDivideBy5(x, y) {
+  return (x * y) / (5);
+}
+
+multiplyAndDivideBy5(5, 5);
+
+// subtractTwoNumbers
+function subtractTwoNumbers(x, y) {
+  return x - y;
+}
+
+subtractTwoNumbers(10, 2);
+
+// getCircleCircumference
+function getCircleCircumference(radius) {
+  return (2 * 3.14) * radius;
+}
+
+getCircleCircumference(5);
+
+// getFullName
+function getFullName(x, y) {
+  return x + y;
+}
+
+getFullName('Bingle', ' Padilla');
+
+// cube
+function cube(number) {
+  return number * number;
+}
+
+cube(8);
