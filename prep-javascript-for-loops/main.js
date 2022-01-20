c20761a5f798cdfefaa2e305a6aaf6c06182315c
codewithbingle <codewithbingle@gmail.com>
2022-01-19 20:28:03 -0800

    Updated

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,12 +1,12 @@
 // Loop 1
-/* for ( let i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
   console.log(i);
-} */
+}
 
 // Loop 2
-/* for (let i = 0; i < 10; i++) {
-  console.log(i * 2)
-} */
+for (let i = 0; i < 10; i++) {
+  console.log(i * 2);
+}
 
 // Loop 3
 for (var i = 100; i >= 1; i--) {
